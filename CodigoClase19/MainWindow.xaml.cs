a3fa7104e90a5785ceaac8c063958f56fb0d9f6e
Hugo Torrico <torricomarquezh@gmail.com>
2024-05-13 19:50:36 -0500

    Agregar archivos de proyecto.

---
+++ CodigoClase19/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CodigoClase19
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Persona> personas = new List<Persona>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, RoutedEventArgs e)
+        {
+            dgDemo.ItemsSource = null;
+            dgDemo.ItemsSource = personas;
+            
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            personas.Add(new Persona
+            {
+                Apellidos = txtApellidos.Text,
+                Nombres = txtNombres.Text,
+                Edad=36                
+            });
+            MessageBox.Show("Registro de forma correcta");
+        }
+        
+    }
+}
---
+++ CodigoClase19/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoClase19
+{
+    public class Persona
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+
+        public int Edad { get; set; }
+    }
+}
